f330238a0941601185957df7d06fdd206243ad59
dhnelson1 <nelson_deborah@bah.com>
2017-06-30 13:35:58 -0400

    added fake data

--- src/app/mini-card/mini-card.component.ts
+++ src/app/mini-card/mini-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-mini-card',
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mini-card.component.css']
 })
 export class MiniCardComponent implements OnInit {
-
+  @Input() fte;
   constructor() { }
 
   ngOnInit() {
+  }
+  seestatlink() {
+    alert("hello world"+this.fte.language)
   }
 
 }
